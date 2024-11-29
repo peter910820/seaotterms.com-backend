ac54c27a9e7a5f37eae6c08f3a6ae8a90647e5d3
peter910820 <peter910820@gmail.com>
2024-11-29 15:36:57 +0800

    get frontend data

--- main.go
+++ main.go
@@ -6,10 +6,18 @@ import (
 	"os"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/joho/godotenv"
 
 	"seaotterms.com-backend/model"
 )
+
+// type FormData struct {
+// 	username      string
+// 	email         string
+// 	password      string
+// 	checkPassword string
+// }
 
 func main() {
 	err := godotenv.Load()
@@ -19,12 +27,22 @@ func main() {
 
 	if os.Getenv("ENV") == "development" {
 		model.Migration()
-		fmt.Println("Successfully")
 	}
-
 	app := fiber.New()
+
+	app.Use(cors.New(cors.Config{AllowOrigins: "http://localhost:8080",
+		AllowMethods: "POST"}))
+
 	app.Static("/", "./public")
 
+	app.Post("/registerHandler", func(c *fiber.Ctx) error {
+		var data map[string]interface{}
+		if err := c.BodyParser(&data); err != nil {
+			log.Fatalf("%v", err)
+		}
+		fmt.Printf("Received data: %+v\n", data)
+		return c.SendString("test")
+	})
 	app.Post("/loginHandler", func(c *fiber.Ctx) error {
 		return c.SendString("test")
 	})
