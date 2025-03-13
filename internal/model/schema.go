d3eb5b17e2de7bf2ae50360386aee3b927aefe6c
peter910820 <peter910820@gmail.com>
2025-03-13 22:32:55 +0800

    add avatar column for users table

--- internal/model/schema.go
+++ internal/model/schema.go
@@ -39,6 +39,7 @@ type User struct {
 	Username   string    `gorm:"NOT NULL unique" json:"username"`
 	Password   string    `gorm:"NOT NULL" json:"-"`
 	Email      string    `gorm:"NOT NULL unique" json:"email"`
+	Avatar     string    `gorm:"NOT NULL; default:''" json:"avatar"`
 	Exp        int       `gorm:"default:0" json:"exp"`
 	Management bool      `gorm:"default:false" json:"management"`
 	CreatedAt  time.Time `gorm:"autoCreateTime" json:"createdAt"`
